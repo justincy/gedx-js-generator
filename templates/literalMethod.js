ce3bf84fea35e7dc8fb530ee25fc8d017db460bd
Justin <justincyork@gmail.com>
2016-10-18 17:06:39 +0000

    Fix literal types

--- templates/literalMethod.js
+++ templates/literalMethod.js
@@ -1,7 +1,7 @@
 /**
  * Get the {{propertyName}}{{#isBoolean type}} flag{{/isBoolean}}
  * 
- * @returns {{type}} {{propertyName}}
+ * @returns { {{~capitalizeFirst type~}} } {{propertyName}}
  */
 {{className}}.prototype.get{{capitalizeFirst propertyName}} = function(){
   return this.{{propertyName}};
@@ -10,7 +10,7 @@
 /**
  * Set the {{propertyName}}
  * 
- * @param {{{type}}} {{propertyName}}
+ * @param { {{~capitalizeFirst type~}} } {{propertyName}}
  * @returns { {{~className~}} } this
  */
 {{className}}.prototype.set{{capitalizeFirst propertyName}} = function({{propertyName}}){
